3e0dd04aeaaa1fc4352fec437a121b2f7a56602a
Heyshmu Digital <info@heyshmu.com>
2018-05-13 22:15:09 +1000

    updated jumbotron

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -9,13 +9,13 @@ export default class Jumbotron extends React.Component {
       }}>
         <div className="mw7 center ph3">
           <div className="db mb3">
-            <div className="mw7 relative bg-fix-primary mb3">
-              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+            <div className="mw7 relative mb3">
+              <h1 className="f2 f1-l b di lh-title mb3 white mw6">
                 { title1 } <br/> { title2 }
               </h1>
             </div>
-            <div className="mw7 relative bg-fix-primary">
-              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ subtitle }</p>}
+            <div className="mw7 relative">
+              {subtitle && <p className="b f4 di lh-title mb3 white mw6">{ subtitle }</p>}
             </div>
           </div>
         </div>

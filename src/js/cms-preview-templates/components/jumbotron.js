e64e26f4a438e18700f003341e67e4cb0c15c043
Heyshmu Digital <info@heyshmu.com>
2018-05-13 22:02:48 +1000

    added new line in title

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class Jumbotron extends React.Component {
   render() {
-    const {image, title, subtitle} = this.props;
+    const {image, title1, title2, subtitle} = this.props;
     return <div>
       <div className="pv5 pv6-l ph3 bg-center cover" style={{
         backgroundImage: image && `url(${image})`
@@ -11,7 +11,7 @@ export default class Jumbotron extends React.Component {
           <div className="db mb3">
             <div className="mw7 relative bg-fix-primary mb3">
               <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
-                { title }
+                { title1 } <br/> { title2 }
               </h1>
             </div>
             <div className="mw7 relative bg-fix-primary">

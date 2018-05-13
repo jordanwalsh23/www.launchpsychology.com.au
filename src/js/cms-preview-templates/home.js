8c38899dd1dc98dd6db32db98489dc0ff8536035
Heyshmu Digital <info@heyshmu.com>
2018-05-13 21:37:52 +1000

    initial changes

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import format from "date-fns/format";
-
 import Jumbotron from "./components/jumbotron";
 
 export default class PostPreview extends React.Component {
@@ -10,11 +8,11 @@ export default class PostPreview extends React.Component {
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
-        image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+      image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
 
     return <div>
-        <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
+        <Jumbotron image={image} title1={entry.getIn(["data", "title1"])} title2={entry.getIn(["data", "title2"])} subtitle={entry.getIn(["data", "subtitle"])}/>
 
         <div className="bg-grey-1 pv4">
           <div className="flex-l mhn1-l ph3 center mw7">
@@ -59,11 +57,8 @@ export default class PostPreview extends React.Component {
             <div className="tc">
               <a href="{{.buttonLink}}" className="btn raise">Read more</a>
             </div>
-
           </div>
         </div>
-
-
-    </div>
+    </div>;
   }
 }
